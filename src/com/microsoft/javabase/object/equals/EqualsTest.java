e002ccb711812eb9319b504fb1b5830137e97939
Spark <wc260188704@gmail.com>
2024-12-11 15:03:00 +0800

    feat(main): init project of upload-to-github

---
+++ src/com/microsoft/javabase/object/equals/EqualsTest.java
@@ -0,0 +1,17 @@
+package com.microsoft.javabase.object.equals;
+
+public class EqualsTest {
+    public static void main(String[] args){
+
+    }
+}
+class Person{
+    private String name;
+    private int age;
+    private char gender;
+    public Person(String name,int age, char gender){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
